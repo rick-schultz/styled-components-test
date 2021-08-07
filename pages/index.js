044a24c2030f4c8a0df950a1f17e0a69678b17d0
Rick Schultz <rick-schultz@hotmail.com>
2021-08-07 10:34:28 -0300

    all done

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
-import styled from 'styled-components'
+import Footer from '../components/Footer';
+import Link from 'next/link';
+import { getAllPosts } from '../infra/getAllPosts';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export default function Home({ allPosts }) {
+  return (
+    <div>
+      <header className="headerContainer">
+        <img src="https://avatars.githubusercontent.com/u/60229666?v=4" />
+        <h1>Rick's Blog</h1>
+      </header>
 
-export default function Home() {
-  return <Title>My page</Title>
+      <section className="postsContainer">
+        {allPosts.map((post) => (
+          <article key={post.slug} className="postsContainer__post">
+            <Link href={`posts/${post.slug}`}>
+              <a>{post.title}</a>
+            </Link>
+            <p>{post.excerpt}</p>
+          </article>
+        ))}
+      </section>
+
+      <Footer
+        facebook="rickschultzofficial"
+        twitter="rick_schultz_"
+        linkedin="rick-schultz"
+        github="rick-schultz"
+      />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const allPosts = await getAllPosts(['title', 'slug', 'excerpt']);
+  return {
+    props: { allPosts },
+  };
 }
